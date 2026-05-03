fefddc3067030912e99d9d793fc72f7ef8e1144a
maks <m_romanets@twistellar.com>
2026-05-03 15:51:17 +0300

    update mapping

--- src/mappings/haulageInstruction.js
+++ src/mappings/haulageInstruction.js
@@ -7,8 +7,8 @@ const fieldPaths = [
   'Grower__c',
   'Contact_Name__c',
   'Telephone__c',
-  'Variety__c',
-  'Commodity__c',
+  'Variety__c.Name',
+  'Commodity__c.Name',
   'Tonnage__c',
   'Buyer__c',
   'Delivery_Instruction__c.Delivery_Reference_No__c',
@@ -20,14 +20,26 @@ function map(data) {
   const root = data.Haulage_Instruction__c || data || {};
   const movementSheet = data.Movement_Sheet__c || root.Movement_Sheet__c || {};
   const deliveryInstruction = data.Delivery_Instruction__c || root.Delivery_Instruction__c || {};
+  const commodity = root.Commodity__c || {};
+  const variety = root.Variety__c || {};
+  const commodityName =
+    commodity.Name ??
+    root.Commodity__r?.Name ??
+    '';
+  const varietyName =
+    variety.Name ??
+    root.Variety__r?.Name ??
+    '';
 
   return {
     'Haulage_Instruction__c.Haulier__c': root.Haulier__c ?? '',
     'Haulage_Instruction__c.Grower__c': root.Grower__c ?? '',
     'Haulage_Instruction__c.Contact_Name__c': root.Contact_Name__c ?? '',
     'Haulage_Instruction__c.Telephone__c': root.Telephone__c ?? '',
-    'Haulage_Instruction__c.Variety__c': root.Variety__c ?? '',
-    'Haulage_Instruction__c.Commodity__c': root.Commodity__c ?? '',
+    'Haulage_Instruction__c.Variety__c.Name': varietyName,
+    'Haulage_Instruction__c.Commodity__c.Name': commodityName,
+    'Haulage_Instruction__c.Variety__c': varietyName,
+    'Haulage_Instruction__c.Commodity__c': commodityName,
     'Haulage_Instruction__c.Tonnage__c': root.Tonnage__c ?? '',
     'Haulage_Instruction__c.Buyer__c': root.Buyer__c ?? '',
     'Haulage_Instruction__c.Haulage_Rate__c': root.Haulage_Rate__c ?? '',
